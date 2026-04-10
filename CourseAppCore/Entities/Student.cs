38578e524bf02c3d3f356a2feca2c20c85a6d6bd
EchosDev <elmirfsh-apa202@code.edu.az>
2026-04-10 15:32:08 +0400

    Fixes all things

---
+++ CourseAppCore/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using CourseAppCore.Common;
+
+namespace CourseAppCore.Entities
+{
+    public class Student : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public CourseGroup Group { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Surname: {Surname}, Age: {Age}, Group: {Group.Name}";
+        }
+    }
+}
---
+++ CourseAppRepository/Repositories/Implementations/GroupRepository.cs
@@ -0,0 +1,69 @@
+﻿using CourseAppCore.Entities;
+using CourseAppRepository.Data;
+using CourseAppRepository.Exceptions;
+using CourseAppRepository.Repositories.Interfaces;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace CourseAppRepository.Repositories.Implementations
+{
+    public class GroupRepository : IRepositrory<CourseGroup>
+    {
+        public void Create(CourseGroup data)
+        {
+            try
+            {
+                if (data is null) throw new NotFoundException("Data not found");
+                AppDbContext<CourseGroup>.datas.Add(data);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error has been detected! Error message: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+        public void Delete(CourseGroup data)
+        {
+            try
+            {
+                if (data is null) throw new NotFoundException("Data not found");
+
+                AppDbContext<CourseGroup>.datas.Remove(data);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error has been detected! Error message: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+        public CourseGroup Get(Predicate<CourseGroup> predicate)
+        {
+            return predicate != null ? AppDbContext<CourseGroup>.datas.Find(predicate) : null;
+        }
+        public List<CourseGroup> GetAll(Predicate<CourseGroup> predicate = null)
+        {
+            return predicate != null ? AppDbContext<CourseGroup>.datas.FindAll(predicate) : AppDbContext<CourseGroup>.datas;
+        }
+        public void Update(CourseGroup data)
+        {
+            try
+            {
+                if (data is null) throw new NotFoundException("Data not found");
+
+                int index = AppDbContext<CourseGroup>.datas.FindIndex(g => g.Id == data.Id);
+
+                if (index == -1) throw new NotFoundException("Group to update not found");
+
+                AppDbContext<CourseGroup>.datas[index] = data;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error has been detected! Error message: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
